2c62bff4d5f0b7effc98756eede92887a8ec9128
ryanvo162 <thuan.vo@executionlab.asia>
2022-11-09 09:03:49 +0700

    Up app

---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+export const primaryColors = {
+    brandGreen: '#00856F',
+    brandGreenLight: '#DCE9E2',
+    brandGreenLight2: '#F7F9F9',
+    brandBeige: '#FFFAF1',
+    brandGreenHover: '#00453A'
+}
+
+export const secondaryColors = {
+    grayHeadline: '#212529',
+    grayParagraph: '#495057',
+    white: '#EDEDED',
+}
